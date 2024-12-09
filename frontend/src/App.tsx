22dc7b1439413ce5e48d8f6862440cfd7977127d
RohaN <166110329+0to1e@users.noreply.github.com>
2024-12-09 20:55:26 +0545

    basic routing added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,14 +5,19 @@ import { ResponsiveHeaderContextProvider } from "./contexts/ResponsiveHeaderCont
 import HomePage from "./pages/Home/HomePage";
 import AuthPage from "./pages/Auth/AuthPage";
 import Test from "./pages/Test";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <ResponsiveHeaderContextProvider>
       <div className="min-h-screen">
-        {/* <HomePage /> */}
-        {/* <Test/> */}
-        <AuthPage/>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/auth" element={<AuthPage />} />
+            <Route path="/test" element={<Test />} />
+          </Routes>
+        </Router>
       </div>
     </ResponsiveHeaderContextProvider>
   );
